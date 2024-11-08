8a08a9778d47dcaae74e5681cfdf791288f3fe0b
Indresh Singh Rathore <indresh2152@gmail.com>
2024-11-08 17:50:00 +0530

    Adding denomination function

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,4 +24,25 @@ describe('AppComponent', () => {
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain('learn-angular app is running!');
   });
+
+  it('should give number of notes for 22 bill amount', () => {
+    const billAmount = 22;
+    const denominations = [1,2,5,10,20];
+    const app = new AppComponent();
+    expect(app.getDenominations(billAmount, denominations)).toBe(2);
+  });
+
+  it('should give number of notes for 17 bill amount', () => {
+    const billAmount = 17;
+    const denominations = [1, 5, 10];
+    const app = new AppComponent();
+    expect(app.getDenominations(billAmount, denominations)).toBe(4);
+  });
+  
+  it('should give number of notes for 10 bill amount', () => {
+    const billAmount = 10;
+    const denominations = [1, 5, 10];
+    const app = new AppComponent();
+    expect(app.getDenominations(billAmount, denominations)).toBe(1);
+  });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'learn-angular';
+
+  getDenominations(bill: number, denominations: any) {
+    let remainder = bill;
+    let sortedDenominations = denominations.sort(this.compare);
+
+    let numberOfNotes = 0
+    if (sortedDenominations.indexOf(remainder) > -1) {
+      numberOfNotes = 1;
+    } else {
+      sortedDenominations?.forEach( (item: number) => {
+        if (item <= remainder) {
+          const quotient = Math.floor(remainder/item); 
+          remainder = remainder%item;
+          numberOfNotes += quotient;
+        }
+      });
+    }
+    return numberOfNotes;
+  }
+
+  // Below compare function will sort in descending order
+  compare(a: number, b: number) {
+    if (a > b) {
+      return -1;
+    } else if (a < b) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
 }
